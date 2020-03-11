e7e438c9745c99db9c1575f6f006b71dc9c6b431
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-03-11 15:53:50 -0400

    Updated barcode api link. Added google books api axios call.

--- public/main.js
+++ public/main.js
@@ -26,22 +26,35 @@ socket.on('motionend', function(motionend){
 
 let barcode = document.querySelector('#barcodeInput');//'9780691158648';//'9780140157376';
 let barcodeButton = document.querySelector('#barcodeButton');
-let isbn;
 
 // finds book by input barcode using barcodable API
 function findBookByBarcode() { 
 	barcode = document.querySelector('#barcodeInput');
-	barcode = barcode.value;
+	barcode = barcode.value;//9781455586691
 	// console.log(barcode);
 
-	let barcode_api = `https://cors-anywhere.herokuapp.com/https://api.barcodelookup.com/v2/products?barcode=${barcode}&formatted=y&key=gzz6z37yi996us4087hmpr2d8pjrnn`;
+	let barcode_api = `https://cors-anywhere.herokuapp.com/https://api.barcodable.com/api/v1/upc/${barcode}`;
+	// let barcode_api = `https://cors-anywhere.herokuapp.com/https://api.barcodelookup.com/v2/products?barcode=${barcode}&formatted=y&key=${key}`;
 	axiosCall(barcode_api, (response) => {
-		let addedBookTitle = document.querySelector('#addedBookTitle');
-		let addedBookImage = document.querySelector('#addedBookImage');
-		let bookTitle = response.data.products[0].title;
-		addedBookTitle.innerHTML = bookTitle; 
-		addedBookImage.src = response.data.products[0].images[0]; 
-		addedBookImage.alt = bookTitle + " image"; 
+			console.log(response.data);
+			let addedBookTitle = document.querySelector('#addedBookTitle');
+			let addedBookImage = document.querySelector('#addedBookImage');
+			// let bookTitle = response.data.products[0].title; //barcodelookup
+			let bookTitle = response.data.item.matched_items[0].title; //barcodeable
+			let isbn = response.data.item.isbn; //barcodeable
+			// console.log(isbn);
+			addedBookTitle.innerHTML = bookTitle; 
+			// addedBookImage.src = response.data.products[0].images[0]; //barcodelookup
+			addedBookImage.alt = bookTitle + " image"; 
+			
+			// retrieve book image and page numbers from google books api 
+			axiosCall(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`, (response) => {
+				console.log('google')
+				console.log(response.data)
+				addedBookImage.src = response.data.items[0].volumeInfo.imageLinks.thumbnail;
+				let pages = response.data.items[0].pageCount;
+				console.log(pages)
+			});
 		// console.log(response.data);
 	} );
 }
