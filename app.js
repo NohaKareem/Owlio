ff1aa0187c213d91fd5cfba610a59a89c99fe0ba
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-04-03 12:05:08 -0400

    Added API route

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var bookRouter = require('./routes/book');
 var sessionRouter = require('./routes/session');
 var settingRouter = require('./routes/setting');
+var apiRouter = require('./routes/api');
 var usersRouter = require('./routes/users');
 
 mongoose.set('useFindAndModify', false);
@@ -49,6 +50,7 @@ app.use('/', indexRouter);
 app.use('/book', bookRouter);
 app.use('/session', sessionRouter);
 app.use('/setting', settingRouter);
+app.use('/api', apiRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -14,42 +14,6 @@ router.get('/test', function(req, res, next) {
   res.render('test', { title: 'Routes Testing' });
 });
 
-// Books
-// API - GET all books
-router.get('/books', (req, res, next) => {
-    Book.find((err, books) => {
-      handleErr(err);
-      res.json(books);
-    });
-});
-
-// Sessions
-// API - GET all sessions (including light data)
-router.get('/sessions', (req, res, next) => {
-  Session.find((err, sessions) => {
-    handleErr(err);
-    res.json(sessions);
-  });
-});
-
-// API - GET most recent sessions
-router.get('/sessions/recent/:recent_sessions_count', (req, res, next) => {
-  let recent_sessions_count = parseInt(req.params.recent_sessions_count);
-  Session.find((err, sessions) => {
-    handleErr(err);
-    res.json(sessions);
-  }).limit(recent_sessions_count).sort({ end_time: 'desc' });
-});
-
-// settings 
-// API - GET all settings (including light data)
-router.get('/settings', (req, res, next) => {
-  Setting.find((err, settings) => {
-    handleErr(err);
-    res.json(settings);
-  });
-});
-
 function handleErr(err) {
   if(err) return next(err);
 }
