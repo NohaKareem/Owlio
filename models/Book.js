30393ab7459b62f3f94d355343d5bbb9a65a6fc8
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-04-08 14:22:14 -0400

    Update book model to include more data

--- models/Book.js
+++ models/Book.js
@@ -5,6 +5,14 @@ var bookSchema = new mongoose.Schema({
     rating: Number, 
     barcode: String, 
     isbn: String, 
+
+    title: String, 
+    author: String, 
+    genre: String, 
+    categories: [String], 
+    pages: Number, 
+    image: String, 
+
     favorite: Boolean
 }); 
 
