92436da53a367b9302a8ae789faf41f204bfcbcb
anitaji23 <57645049+anitaji23@users.noreply.github.com>
2020-04-13 12:48:10 -0400

    index.js modified and updated firmata version

--- index.js
+++ index.js
@@ -32,43 +32,67 @@ arduino.on('ready', function(){
     });
 
 //motion
-    proximity = new five.Proximity({
-        controller: "HCSR04",
-        pin: 7,
-        freq: 1000
+            // proximity = new five.Proximity({
+            //     controller: "HCSR04",
+            //     pin: 7,
+            //     freq: 1000
+            // });
+
+            // proximity.on("data", function() {
+            //     console.log(this.cm + "cm", this.in + "in");
+            // });
+            
+            // proximity.on("change", function() {
+            //     if (proximity.cm < 35) {
+            //         console.log("Someone's there!");
+            //         io.sockets.emit('motionstart', "Someone's there!");
+            //     } 
+            //     else {
+            //         console.log("Oops! no one's there.");
+            //         io.sockets.emit('motionend', "Oops! no one's there.");
+            //     }
+            // });
+
+
+// neopixel lights
+    strip = new pixel.Strip({
+        board: this,
+        controller: "FIRMATA",
+        strips: [ {pin: 7, length: 12}, ],
+        gamma: 2.8,
     });
 
-    // proximity.on("data", function() {
-        // console.log(this.cm + "cm", this.in + "in");
+    strip.on("ready", function() {
+        console.log("light up")
+        // Set the entire strip to pink.
+        strip.color('#903');
+
+        // Set first and seventh pixels to turquoise.
+        strip.pixel(0).color('#074');
+        strip.pixel(6).color('#074');
+
+        // Send instructions to NeoPixel.
+        strip.show();
+
+        // var loop = setInterval(function () {
+        //     // Shift all pixels clockwise
+        //     strip.shift(1, pixel.FORWARD, true);
+        //     strip.show();
+        // }, 1000 / 12);
+    });
+
+    this.repl.inject({
+        strip: strip
+    });
+
+    // strip.on("ready", function() {
+    //     console.log("light up")
+
+    //     for (i = 0; i < strip.stripLength(); i++) {
+    //         console.log(i);
+    //         strip.pixel(i).color("teal");
+    //     }
+    //     strip.show();
     // });
-    
-    proximity.on("change", function() {
-        if (proximity.cm < 35) {
-            console.log("Someone's there!");
-            io.sockets.emit('motionstart', "Someone's there!");
-        } 
-        else {
-            console.log("Oops! no one's there.");
-            io.sockets.emit('motionend', "Oops! no one's there.");
-        }
-    });
-
-//neopixel lights
-// strip = new pixel.Strip({
-//     data: 6,
-//     length: 12,
-//     board: this,
-//     controller: "FIRMATA",
-// });
-
-// strip.on("ready", function() {
-//     console.log("light up")
-
-//     for (i = 0; i < strip.stripLength(); i++) {
-//         console.log(i);
-//         strip.pixel(i).color("teal");
-//     }
-//     strip.show();
-// });
     
 }); 
